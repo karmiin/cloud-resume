b07e5723041685de2a9b00fe3df94bf0e7550220
karmiin <karmintng@gmail.com>
2025-11-26 17:51:59 +0100

    new resource record

--- main.tf
+++ main.tf
@@ -281,6 +281,17 @@ resource "aws_route53_record" "www" {
     evaluate_target_health = false
   }
 }
+resource "aws_route53_record" "apex" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "karmin.dev"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
 provider "aws" {
   alias = "virginia"
   region = "us-east-1"
